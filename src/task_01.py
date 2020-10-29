e655da991996e14a70caf3bbb721445517fe1e57
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-10-28 22:10:27 -0400

    Solved task 01

---
+++ src/task_01.py
@@ -0,0 +1,44 @@
+"""
+Write a function that searches a list of names (unsorted) for the name "Bob" and returns the location in the list. If Bob is not in the array, return -1.
+
+Examples:
+
+csWhereIsBob(["Jimmy", "Layla", "Bob"]) ➞ 2
+csWhereIsBob(["Bob", "Layla", "Kaitlyn", "Patricia"]) ➞ 0
+csWhereIsBob(["Jimmy", "Layla", "James"]) ➞ -1
+Notes:
+
+Assume all names start with a capital letter and are lowercase thereafter (i.e. don't worry about finding "BOB" or "bob").
+[execution time limit] 4 seconds (py3)
+
+[input] array.string names
+
+[output] integer
+
+[Python 3] Syntax Tips
+
+# Prints help message to the console
+# Returns a string
+def helloWorld(name):
+    print("This prints to the console when you Run Tests")
+    return "Hello, " + name
+
+"""
+
+# def csWhereIsBob(names):
+#     for x in names:
+#         if x == "Bob":
+#             return names.index(x)
+#         else:
+#             return -1
+
+def csWhereIsBob(names):
+    for x in names:
+        if x == "Bob":
+            return names.index(x)
+        else:
+            return -1
+
+
+print(csWhereIsBob(["Jimmy", "Layla", "Mandy"]))
+print(csWhereIsBob(["Bob", "Nathan", "Hayden"]))
